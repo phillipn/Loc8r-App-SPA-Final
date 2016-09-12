d5498fd429fcee668ef9b81375bba899ee2e908d
Nick Phillips <phillipn101@gmail.com>
2016-09-12 14:37:42 -0500

    Authentication via passport

---
+++ app_client/locationDetail/locationDetail.controller.js
@@ -0,0 +1,47 @@
+(function () {
+
+  angular
+    .module('loc8rApp')
+    .controller('locationDetailCtrl', locationDetailCtrl);
+
+  locationDetailCtrl.$inject = ['$routeParams', '$location', '$uibModal', 'authentication', 'loc8rData'];
+  function locationDetailCtrl ($routeParams, $location, $uibModal, authentication, loc8rData) {
+    console.log(vm);
+    var vm = this;
+    vm.locationid = $routeParams.locationid;
+    
+    vm.isLoggedIn = authentication.isLoggedIn();
+    vm.currentPath = $location.path();
+    
+loc8rData.locationById(vm.locationid)
+      .success(function(data) {
+        vm.data = { location: data };
+        vm.pageHeader = {
+          title: vm.data.location.name
+        };
+      })
+      .error(function (e) {
+        console.log(e);
+      });
+
+    vm.popupReviewForm = function () {
+      var modalInstance = $uibModal.open({
+        templateUrl: '/reviewModal/reviewModal.view.html',
+        controller: 'reviewModalCtrl as vm',
+        resolve : {
+          locationData : function () {
+            return {
+              locationid : vm.locationid,
+              locationName : vm.data.location.name
+            };
+          }
+        }
+      });
+
+      modalInstance.result.then(function (data) {
+        vm.data.location.reviews.push(data);
+      });
+    };
+  }
+
+})();
---
+++ app_server/routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+// var ctrlLocations = require('../controllers/locations');
+var ctrlOthers = require('../controllers/others');
+
+
+router.get('/', ctrlOthers.angularApp);
+// router.get('/location/:locationid', ctrlLocations.locationInfo);
+// router.get('/location/:locationid/reviews/new', ctrlLocations.addReview);
+// router.post('/location/:locationid/reviews/new', ctrlLocations.doAddReview);
+
+// router.get('/about', ctrlOthers.about);
+
+module.exports = router;
